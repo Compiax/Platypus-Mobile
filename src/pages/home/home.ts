9f8e6dabc2aac216d9b4f73a74ef9ae3affb32d7
Duart <duartbreedt@gmail.com>
2017-06-08 22:20:50 +0200

    feat(HomePage): Added structure & access to user data
    Added basic structure to page
    Added access to persisted user preferences

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+// Used for storing user data locally
+import { Storage } from '@ionic/storage';
+
 @IonicPage()
 @Component({
   selector: 'page-home',
@@ -8,10 +11,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(private navCtrl: NavController, private navParams: NavParams) { }
+  nickname: string;
+  colour: string;
+
+  constructor(private navCtrl: NavController, private navParams: NavParams, private storage: Storage) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
+  }
+
+  ionViewWillEnter() {
+
+    this.storage.get('nickname').then((data) => {this.nickname = data;});
+    this.storage.get('colour').then((data) => {this.colour = data;});
+
   }
 
 }
