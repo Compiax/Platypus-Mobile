76b3b7e166eb92170e40d01508b80e1ddc2a71e3
Wanrick <wanrick.pro@gmail.com>
2017-07-10 17:29:30 +0200

    feat(communication): Fixed URL for API communication

--- src/providers/HttpProvider.ts
+++ src/providers/HttpProvider.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-const URL = 'http://localhost:3000/test';
+const URL = 'http://localhost:3000/mobile';
 
 @Injectable()
 export class HttpProvider {
