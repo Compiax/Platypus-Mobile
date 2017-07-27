adf010f42fd8519e37213a8e33e93d08dfdf41ef
Duart <duartbreedt@gmail.com>
2017-07-27 19:47:02 +0200

    Testing modals

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, ModalController, NavController, NavParams, ViewController } from 'ionic-angular';
 
 // Used for storing user data locally
 import { Storage } from '@ionic/storage';
@@ -14,9 +14,10 @@ export class HomePage {
 
   nickname: string;
   colour: string;
+  modal: any;
   pages = PAGES;
 
-  constructor(private navCtrl: NavController, private navParams: NavParams, private storage: Storage) { }
+  constructor(private modalCtrl: ModalController, private navCtrl: NavController, private navParams: NavParams, private storage: Storage) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
@@ -33,6 +34,26 @@ export class HomePage {
    * Opens a page with the specified name
    * @param {String} page The name of the page to open
    */
+  openModal(page): void {
+    // this.modal = this.modalCtrl.create(Profile);
+    this.modal.present();
+  }
+
   openPage(page): void { this.navCtrl.push(page); }
 
+}
+
+@Component({
+  selector: 'page-home',
+  template: '<ion-header><ion-navbar><ion-title>Profile</ion-title></ion-navbar></ion-header><ion-content padding text-center><h1>Profile</h1></ion-content>',
+})
+class Profile {
+
+   constructor(public navParams: NavParams, private viewCtrl: ViewController) {
+
+   }
+
+   dismiss() {
+     this.viewCtrl.dismiss();
+   }
 }
