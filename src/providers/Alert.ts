45fc890620678c45c6dd6c7193b81ac45fa31384
Duart <duartbreedt@gmail.com>
2017-08-17 20:46:09 +0200

    feat(AlertProvider): Created alert functionality

--- src/providers/Alert.ts
+++ src/providers/Alert.ts
@@ -9,4 +9,16 @@ export class Alert {
     private navParams: NavParams,
     private loadingCtrl: LoadingController) {}
 
+    public sendAlert(alertMessage) {
+      this.navCtrl.setRoot("SessionPage").then( function (){
+      let toast = this.toastCtrl.create({
+        message: alertMessage,
+        duration: 5000,
+        showCloseButton: true,
+        closeButtonText: "Ok",
+        position: 'top'
+      });
+    });
+    }
+
 }
