142e58f87a212dce9ce24eb46479980453ca1cec
Duart <duartbreedt@gmail.com>
2017-09-13 11:49:25 +0200

    feat(SessionPage): Conflict merge fix

--- src/providers/HttpProvider.ts
+++ src/providers/HttpProvider.ts
@@ -3,8 +3,6 @@ import { HTTP } from '@ionic-native/http';
 import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
 import { File } from '@ionic-native/file';
 
-import * as io from 'socket.io-client';
-
 // API server URL
 const IP = 'http://192.168.43.144';
 const URL = IP+':3000/mobile';
@@ -14,38 +12,10 @@ const HEADERS = {'Content-Type': 'application/x-www-form-urlencoded'};
 export class HttpProvider {
 
   fileTransfer: FileTransferObject;
-  socket: SocketIOClient.Socket;
 
   constructor(private http: HTTP, private transfer: FileTransfer, private file: File) {
     console.log("Http Provider Instantiated");
     this.fileTransfer = this.transfer.create();
-    this.socket = io(IP+':3002');
-    this.handleListeners();
-  }
-
-  handleListeners() {
-    this.socket.on('sendItem', this.getItem);
-  }
-
-  claimItem(session_id, user_id, quantity, item_id) {
-    this.socket.emit('claimItem', { session_id: session_id, user_id: user_id, quantity: quantity, item_id: item_id });
-  }
-
-  createItem(session_id, price, name, quantity) {
-    this.socket.emit('createItem', { session_id: session_id, price: price, name: name, quantity: quantity });
-  }
-
-  deleteItem(session_id, item_id) {
-    this.socket.emit('deleteItem', { session_id: session_id, item_id: item_id });
-  }
-
-  editItem(session_id, price, name, quantity, item_id) {
-    this.socket.emit('editItem', { session_id: session_id, price: price, name: name, quantity: quantity, item_id: item_id });
-  }
-
-  getItem(data) {
-      console.log("Got item: "+data);
-      // @todo Check if the item in data exists (ID). If it does replace it's information. Else add it to the items.
   }
 
   /**
